a3b485f46e80e4be26c7d20e584c28cfd5841c9f
Anthony Ngene <tonymontaro@Anthonys-MacBook-Pro.local>
2023-07-02 09:43:57 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
-//#include <atcoder/all>
-//using namespace atcoder;
+// #include <atcoder/all>
+// using namespace atcoder;
 using namespace std;
 
+void fastInputOutput() { cin.tie(nullptr)->sync_with_stdio(false); }
 
-void fastInputOutput(){
-    cin.tie(nullptr)->sync_with_stdio(false);
-}
-
-#define usefile() ifstream cin("../test_input.txt"); /*ofstream cout("../out.txt");*/ std::cin.rdbuf(cin.rdbuf()); std::cout.rdbuf(cout.rdbuf())
+#define usefile()                                                     \
+  ifstream cin("../test_input.txt"); /*ofstream cout("../out.txt");*/ \
+  std::cin.rdbuf(cin.rdbuf());                                        \
+  std::cout.rdbuf(cout.rdbuf())
 
 string delim = " ";
 #define print(x) cout << x << endl
@@ -17,23 +17,23 @@ string delim = " ";
 
 template <typename T>
 void printArr(T &arr) {
-    for (auto x: arr) cout << x << " ";
-    cout << endl;
+  for (auto x : arr) cout << x << " ";
+  cout << endl;
 }
 template <typename T>
 void printArr(vector<vector<T>> &arr) {
-    for (auto &x: arr) printArr(x);
+  for (auto &x : arr) printArr(x);
 }
 
 string bin_string(long long num) {
-    if (num == 0) return "0";
-    string res = "";
-    while (num > 0) {
-        res += (num & 1) + '0';
-        num >>= 1;
-    }
-    reverse(res.begin(), res.end());
-    return res;
+  if (num == 0) return "0";
+  string res = "";
+  while (num > 0) {
+    res += (num & 1) + '0';
+    num >>= 1;
+  }
+  reverse(res.begin(), res.end());
+  return res;
 }
 
 #define ll long long
@@ -41,69 +41,65 @@ string bin_string(long long num) {
 const int INF = (int)2e9;
 const ll LINF = (ll)4e18;
 ll mpow(ll x, ll y, ll p) {
-    ll res = 1;
-    if (y == 0) return 1; x = x % p;  if (x == 0) return 0; // In case x is divisible by p;
-    while (y > 0) {
-        if (y & 1)
-            res = (res*x) % p;
-        y = y>>1; // y = y/2
-        x = (x*x) % p;
-    }
-    return res;
+  ll res = 1;
+  if (y == 0) return 1;
+  x = x % p;
+  if (x == 0) return 0;  // In case x is divisible by p;
+  while (y > 0) {
+    if (y & 1) res = (res * x) % p;
+    y = y >> 1;  // y = y/2
+    x = (x * x) % p;
+  }
+  return res;
 }
 
-ll popcount(ll num) {
-    return __builtin_popcount((unsigned int) num);
-}
+ll popcount(ll num) { return __builtin_popcount((unsigned int)num); }
 ll MOD = 1e9 + 7;
 
-ll hashNum(ll a) {
-    return a * (a + 1346) * (a + 9185);
-}
+ll hashNum(ll a) { return a * (a + 1346) * (a + 9185); }
 void extendVector(vector<ll> &v, vector<ll> &other) {
-    for (auto val: other) v.push_back(val);
+  for (auto val : other) v.push_back(val);
 }
 map<ll, ll> coordCompress(vector<ll> &arr) {
-    set<ll> st(arr.begin(), arr.end());
-    map<ll, ll> mp;
-    ll idx = 1;
-    for (auto &x: st) mp[x] = idx++;
-    return mp;
+  set<ll> st(arr.begin(), arr.end());
+  map<ll, ll> mp;
+  ll idx = 1;
+  for (auto &x : st) mp[x] = idx++;
+  return mp;
 }
 
 #define tp tuple<ll, ll, ll>
-vector<vector<ll>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // right, down, left, up
-vector<vector<ll>> dirsDiag = {{0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
+vector<vector<ll>> dirs = {
+    {0, 1}, {1, 0}, {0, -1}, {-1, 0}};  // right, down, left, up
+vector<vector<ll>> dirsDiag = {{0, 1},  {1, 1},   {1, 0},  {1, -1},
+                               {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
 ll strHashMult = 911382323;
 ll strHashMod = 972663749;
 
-//using lm = ::atcoder::modint1000000007;
+// using lm = ::atcoder::modint1000000007;
 #define ll long long
 #define pi pair<ll, ll>
 // get the bit set value at index idx
 #define bit(num, idx) ((num >> idx) & 1)
 void printArr(vector<pi> &arr) {
-    for (auto x: arr) cout << "(" << x.first << "," << x.second << ") ";
-    cout << endl;
+  for (auto x : arr) cout << "(" << x.first << "," << x.second << ") ";
+  cout << endl;
 }
 ll Combination(ll n, ll k) {
-    long double res = 1;
-    for (int i = 1; i <= k; ++i)
-        res = res * (n - k + i) / i;
-    return (ll)(res + 0.01);
+  long double res = 1;
+  for (int i = 1; i <= k; ++i) res = res * (n - k + i) / i;
+  return (ll)(res + 0.01);
 }
 vector<string> splitWord(string &s) {
-    istringstream iss(s, istringstream::in);
-    vector<string> words;
-    string word;
-    while (iss >> word) words.push_back(word);
-    return words;
+  istringstream iss(s, istringstream::in);
+  vector<string> words;
+  string word;
+  while (iss >> word) words.push_back(word);
+  return words;
 }
 
-//using lm = ::atcoder::modint1000000007;
-//using lm = ::atcoder::modint998244353;
-
-
+// using lm = ::atcoder::modint1000000007;
+// using lm = ::atcoder::modint998244353;
 
 #define ll long long
 #define lb long double
@@ -111,20 +107,16 @@ vector<string> splitWord(string &s) {
 #define tpp tuple<ll, ll, ll>
 
 class Solution {
-public:
-    void run() {
-        print("ready");
-    }
+ public:
+  void run() {}
 };
 
+int main() {
+  fastInputOutput();
+  //    usefile();
 
-int main(){
-    fastInputOutput();
-//    usefile();
+  Solution sol;
+  sol.run();
 
-    Solution sol;
-    sol.run();
-
-    return 0;
+  return 0;
 }
-
